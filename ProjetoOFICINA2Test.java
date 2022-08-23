c7feba9facc5aa757e59dc26a1c23c8876137204
Guilherme Chizzolini Andreotti <111712600+GuilhermeAndreotti@users.noreply.github.com>
2022-08-23 17:14:13 -0300

    Update ProjetoOFICINA2Test.java

--- ProjetoOFICINA2Test.java
+++ ProjetoOFICINA2Test.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
- */
 package com.mycompany.projetooficina2;
 
 import org.junit.jupiter.api.AfterEach;
@@ -11,10 +7,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author Guilherme Andreotti
- */
 public class ProjetoOFICINA2Test {
     
     public ProjetoOFICINA2Test() {
